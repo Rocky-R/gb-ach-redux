0e3e95b9e564dabdb56d468c5f6936efb1a38c99
Rocky Randalls <rrandalls@gmail.com>
2015-02-05 15:02:45 -0500

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def log_in
+  end
+
+  def log_out
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/log_in'
+
+  get 'sessions/log_out'
+
   get 'grades/index'
 
   get 'grades/new'
