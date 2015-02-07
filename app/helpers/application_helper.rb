1af7936986bebccf79c22a635b8971f82a4aed49
Rocky Randalls <rrandalls@gmail.com>
2015-02-07 10:52:44 -0500

    Add grade view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def grade_view_filter
+    @user = Teacher.find_by_id(session[:user_id]) ||
+            Student.find_by_id(session[:user_id]) ||
+            Parent.find_by_id(session[:user_id])
+    if session[:user_id] == Teacher.find_by_id(session[:user_id])
+      @grades = Grade.all
+    elsif session[:user_id] == Student.find_by_id(session[:user_id])
+      @grades = @user.grades
+    elsif session[:user_id] == Parent.find_by_id(session[:user_id])
+      @grades = @user.student.grades
+    end
+  end
 end
