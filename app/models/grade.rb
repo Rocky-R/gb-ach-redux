5dcdb7a1dbc2a816f54387ed5957e4deef559910
Rocky Randalls <rrandalls@gmail.com>
2015-02-09 10:46:01 -0500

    Rework grades view, add Grade#teacher_name and student_name #shipit

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,19 @@
 class Grade < ActiveRecord::Base
   belongs_to :student
+
+  def student_name
+    if student
+      student.name
+    else
+      ""
+    end
+  end
+
+  def teacher_name
+    if student && student.teacher
+      student.teacher.name
+    else
+      ""
+    end
+  end
 end
